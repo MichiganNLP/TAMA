a46835d9f5d3b1e141bf771c1f9262a8eb4fec1b
Naihao Deng <dnaihao@umich.edu>
2025-06-15 17:41:29 +0000

    Added webpage

--- datasets/gsm8k/raw/download.py
+++
@@ -1,6 +0,0 @@
-from datasets import load_dataset
-
-dataset = load_dataset("openai/gsm8k", "main")
-
-dataset["train"].to_csv("train.csv")  # For the training set
-dataset["test"].to_csv("test.csv")
